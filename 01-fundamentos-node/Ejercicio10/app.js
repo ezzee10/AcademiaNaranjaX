f0b82b1c8ab62dc1f3c53c57a878bd80a5321f31
Ezequiel Colombano <ecolombano@proveedor.teco.com.ar>
2021-07-20 19:39:46 -0300

    Se agregó ESLint al proyecto y se corrigieron los archivos en el ejercicio 10 y en el helper de multiplicar

--- 01-fundamentos-node/Ejercicio10/app.js
+++ 01-fundamentos-node/Ejercicio10/app.js
@@ -1,18 +1,16 @@
-const crearArchivo = require("../helpers/multiplicar");
-const yargs = require("../config/yargs");
+const crearArchivo = require('../helpers/multiplicar')
+const yargs = require('../config/yargs')
 
-const tabla_yargs = () => {
-
-    if (yargs.listar) {
-        crearArchivo(yargs.base, true)
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
-    } else {
-        crearArchivo(yargs.base)
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
-    }
-
+const tablaYargs = () => {
+  if (yargs.listar) {
+    crearArchivo(yargs.base, true)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  } else {
+    crearArchivo(yargs.base)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
 }
 
-tabla_yargs();
+tablaYargs()
--- 01-fundamentos-node/helpers/multiplicar.js
+++ 01-fundamentos-node/helpers/multiplicar.js
@@ -1,31 +1,24 @@
 
-/*Función que genera un archivo .txt*/
+/* Función que genera un archivo .txt */
 const crearArchivo = (numero = 5, imprimir = false) => {
+  const fs = require('fs')
+  const colors = require('colors')
 
-    let fs = require('fs');
-    let colors = require('colors');
+  return new Promise((resolve, reject) => {
+    if (numero < 0 || !Number.isInteger(numero) || isNaN(numero)) {
+      throw new Error('El número ingresado debe ser entero positivo mayor o igual que 0')
+    } else {
+      let resultado = ''
 
-    return new Promise((resolve, reject) => {
-
-        if (numero < 0 || !Number.isInteger(numero) || isNaN(numero)) {
-            reject('El número ingresado debe ser entero positivo mayor o igual que 0');
-            return;
-        }
-        else {
-            let resultado = '';
-
-            for (let i = 1; i <= 10; i++) {
-                resultado += `${numero} * ${i} = ${numero * i} \n`
-            }      
-
-            resultado_consola = colors.green(`*** Tabla del ${numero} *** \n \n`) + colors.red(resultado);
-            
-            fs.writeFile(`salida/tabla-${numero}.txt`, resultado, function(err) {
-                if (err) reject(err);
-                else resolve(imprimir ? resultado_consola : `tabla-${numero}.txt creado correctamente`);
-            });
-        }
-    });
-};
-
+      for (let i = 1; i <= 10; i++) {
+        resultado += `${numero} * ${i} = ${numero * i} \n`
+      }
+      const resultadoConsola = colors.green(`*** Tabla del ${numero} *** \n \n`) + colors.red(resultado)
+      fs.writeFile(`salida/tabla-${numero}.txt`, resultado, function (err) {
+        if (err) reject(err)
+        else resolve(imprimir ? resultadoConsola : `tabla-${numero}.txt creado correctamente`)
+      })
+    }
+  })
+}
 module.exports = crearArchivo
